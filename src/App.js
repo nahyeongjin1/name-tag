08bb1a7fc688a743e4c34c61282f70caf79375b1
나형진 <skgudwls@konkuk.ac.kr>
2024-09-10 10:38:18 +0900

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="name-tag">
+        <h1>나형진</h1>
+        <div className="underline"></div>
+        <p>컴퓨터공학부</p>
+        <p className="student-id">202011285</p>
+      </div>
     </div>
   );
 }
